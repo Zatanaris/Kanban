b728f7eaae7427ea495510f38c84ba8e2cacf966
Douglas Rodrigues <123272282+Zatanaris@users.noreply.github.com>
2023-04-01 19:36:19 -0300

    iniada devlope

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,1 @@
-const sidebar = document.querySelector(".sidebar");
-const btn = document.querySelector("#close-bar");
 
-btn.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-    btn.classList.toggle("bx-x");
-});
